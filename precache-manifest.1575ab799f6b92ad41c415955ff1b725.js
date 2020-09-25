6a20855c876b3dd81aa6636af5c4bb59e2dde774
hidayat26 <mdhidayat9163@gmail.com>
2020-09-25 16:51:29 +0530

    Updates

---
+++ precache-manifest.1575ab799f6b92ad41c415955ff1b725.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc60d3a24ab46be2c3a1f4260d75800d",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "72df2846eb2bf97c16cd",
+    "url": "/todoapp/static/css/main.7c0bce59.chunk.css"
+  },
+  {
+    "revision": "eb529bf80e09ca01531c",
+    "url": "/todoapp/static/js/2.dd330b9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.dd330b9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72df2846eb2bf97c16cd",
+    "url": "/todoapp/static/js/main.222b791e.chunk.js"
+  },
+  {
+    "revision": "1262b7d7e3074de6c22b",
+    "url": "/todoapp/static/js/runtime-main.e5516e57.js"
+  }
+]);
